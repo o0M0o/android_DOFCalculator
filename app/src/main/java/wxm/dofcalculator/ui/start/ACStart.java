e4ef29b24bda823969c5217b78772fd6bea4f237
hugo <fly_in_wind@qq.com>
2017-03-10 16:34:50 +0800

    start project

---
+++ app/src/main/java/wxm/dofcalculator/ui/start/ACStart.java
@@ -0,0 +1,15 @@
+package wxm.dofcalculator.ui.start;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import wxm.dofcalculator.R;
+
+public class ACStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac_start);
+    }
+}
