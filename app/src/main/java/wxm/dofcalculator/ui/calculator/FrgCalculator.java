c4268f12166ae03c97f2de21d9041918d32f5cd2
hugo <fly_in_wind@qq.com>
2017-03-18 11:47:03 +0800

    start project

--- app/src/main/java/wxm/dofcalculator/ui/calculator/FrgCalculator.java
+++ app/src/main/java/wxm/dofcalculator/ui/calculator/FrgCalculator.java
@@ -11,6 +11,8 @@ import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import java.math.BigDecimal;
+import java.util.Locale;
 import java.util.NavigableMap;
 import java.util.TreeMap;
 
@@ -19,7 +21,11 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.wxm.andriodutillib.FrgUtility.FrgUtilityBase;
 import wxm.dofcalculator.R;
+import wxm.dofcalculator.define.DeviceItem;
+import wxm.dofcalculator.define.GlobalDef;
+import wxm.dofcalculator.define.LensItem;
 import wxm.dofcalculator.ui.device.ACDevice;
+import wxm.dofcalculator.utility.ContextUtil;
 
 /**
  * first frg for app
@@ -39,9 +45,19 @@ public class FrgCalculator extends FrgUtilityBase {
     @BindView(R.id.tv_lens_aperture_val)
     TextView    mTVLensAperture;
 
+    @BindView(R.id.tv_object_distance_val)
+    TextView    mTVObjectDistance;
 
+    @BindView(R.id.tv_lens_focal_val)
+    TextView    mTVLensFocal;
+
+    private DeviceItem mDICurDevice;
+
+    private final static NavigableMap<Integer, Integer> mNMLensFocal = new TreeMap<>();
+    private final static NavigableMap<Integer, BigDecimal> mNMObjectDistance = new TreeMap<>();
     private final static NavigableMap<Integer, String> mNMLensAperture = new TreeMap<>();
     static {
+        // for lens aperture
         mNMLensAperture.put(0, "F/1.2");
         mNMLensAperture.put(8, "F/1.4");
         mNMLensAperture.put(16, "F/2.0");
@@ -55,6 +71,16 @@ public class FrgCalculator extends FrgUtilityBase {
         mNMLensAperture.put(80, "F/32");
         mNMLensAperture.put(88, "F/44");
         mNMLensAperture.put(100, "F/64");
+
+        // for object distance
+        mNMObjectDistance.put(0, BigDecimal.ZERO);
+        mNMObjectDistance.put(15, BigDecimal.valueOf(0.1));
+        mNMObjectDistance.put(30, BigDecimal.valueOf(1.5));
+        mNMObjectDistance.put(45, BigDecimal.valueOf(2));
+        mNMObjectDistance.put(60, BigDecimal.valueOf(3));
+        mNMObjectDistance.put(75, BigDecimal.valueOf(5));
+        mNMObjectDistance.put(90, BigDecimal.valueOf(10));
+        mNMObjectDistance.put(100, BigDecimal.valueOf(25));
     }
 
 
@@ -89,7 +115,9 @@ public class FrgCalculator extends FrgUtilityBase {
          * 处理lens focal
          * @param sb  for seekbar
          */
-        private void onLensFocal(SeekBar sb)    {
+        private void onLensFocal(SeekBar sb) {
+            int p = sb.getProgress();
+            updateLensFocal(p);
         }
 
         /**
@@ -98,8 +126,7 @@ public class FrgCalculator extends FrgUtilityBase {
          */
         private void onLensAperture(SeekBar sb)    {
             int p = sb.getProgress();
-            String sz_hot = mNMLensAperture.floorEntry(p).getValue();
-            mTVLensAperture.setText(sz_hot);
+            updateLensAperture(p);
         }
 
         /**
@@ -107,6 +134,8 @@ public class FrgCalculator extends FrgUtilityBase {
          * @param sb  for seekbar
          */
         private void onObjDistanc(SeekBar sb)    {
+            int p = sb.getProgress();
+            updateObjectDistance(p);
         }
     };
 
@@ -121,6 +150,23 @@ public class FrgCalculator extends FrgUtilityBase {
     @Override
     protected void initUiComponent(View view) {
         mSBLensAperture.setOnSeekBarChangeListener(mSBChangeListener);
+        mSBLensFocal.setOnSeekBarChangeListener(mSBChangeListener);
+        mSBObjectDistance.setOnSeekBarChangeListener(mSBChangeListener);
+
+        int d_id = getArguments().getInt(ACCalculator.KEY_DEVICE_ID, GlobalDef.INT_INVAILED_ID);
+        if(GlobalDef.INT_INVAILED_ID != d_id)   {
+            mDICurDevice = ContextUtil.getDUDevice().getData(d_id);
+        }
+
+        if(null != mDICurDevice)    {
+            LensItem li = mDICurDevice.getLens();
+            initLensFocalTMap(li.getMinFocal(), li.getMaxFocal());
+        }
+
+        // init show
+        updateLensFocal(0);
+        updateLensAperture(0);
+        updateObjectDistance(0);
     }
 
     @Override
@@ -128,5 +174,48 @@ public class FrgCalculator extends FrgUtilityBase {
     }
 
 
+    /**
+     * 更新lens focal
+     * @param progress  进度
+     */
+    private void updateLensFocal(int progress)   {
+        int f_hot = mNMLensFocal.floorEntry(progress).getValue();
+        String sz_hot = String.format(Locale.CHINA, "%dmm", f_hot);
+        mTVLensFocal.setText(sz_hot);
+    }
 
+    /**
+     * 更新lens aperture
+     * @param progress  进度
+     */
+    private void updateLensAperture(int progress)   {
+        String sz_hot = mNMLensAperture.floorEntry(progress).getValue();
+        mTVLensAperture.setText(sz_hot);
+    }
+
+    /**
+     * 更新object distance
+     * @param progress  进度
+     */
+    private void updateObjectDistance(int progress)   {
+        BigDecimal d_hot = mNMObjectDistance.floorEntry(progress).getValue();
+        String sz_hot = String.format(Locale.CHINA, "%.02fm", d_hot.floatValue());
+        mTVObjectDistance.setText(sz_hot);
+    }
+
+
+    /**
+     * 初始化镜头焦距步进值
+     * @param minFocal      最小焦距
+     * @param maxFocal      最大焦距
+     */
+    private void initLensFocalTMap(int minFocal, int maxFocal)  {
+        mNMLensFocal.clear();
+        int step_val = 10;
+        int step_count = 10;
+        int step_focal = (maxFocal - minFocal)/ step_count;
+        for(int i = 0; i < step_count; ++i) {
+            mNMLensFocal.put(step_val * i, minFocal + step_focal * i);
+        }
+    }
 }
